e4330e02f597a94c9c8967b02d9bd396d0bb7454
yuio7279 <yuio7279@gmail.com>
2024-02-25 02:48:59 +0900

    init commit

---
+++ src/test/java/com/structure/motors/MotorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.structure.motors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
